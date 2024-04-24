11a054a81112c27d5f12f16503c9a8b48506be60
MaxKtv <maximkutiaev@outlook.com>
2024-04-24 17:10:54 +1000

    fix generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,51 +1,44 @@
 from gendiff.parser import get_data
-from gendiff.formatters import plain, stylish
+from gendiff.formatters import stylish, plain
 from pathlib import Path
+from typing import Dict, Any
 
 
-def get_diff(d1: dict, d2: dict) -> tuple:
-    diff = dict()
-    flat_diff = list()
-    for key in sorted(set(d1) | set(d2)):
-        val1, val2 = d1.get(key), d2.get(key)
-        if isinstance(val1, dict) and isinstance(val2, dict):
-            nested_diff, nested_flat_diff = get_diff(val1, val2)
+def get_meta(data1: Dict[str, Any], data2: Dict[str, Any]) -> str:
+    if isinstance(data1, dict) and isinstance(data2, dict):
+        return 'nested'
+    elif data1 == data2:
+        return 'original'
+    elif data1 != data2:
+        return 'changed'
+
+
+def get_diff(data1: Dict[str, Any], data2: Dict[str, Any]) -> Dict[str, Any]:
+    diff = {}
+    for key in sorted(set(data1) | set(data2)):
+        val1, val2 = data1.get(key), data2.get(key)
+        if get_meta(val1, val2) == 'nested':
+            nested_diff = get_diff(val1, val2)
             if nested_diff:
                 diff[key] = nested_diff
-                flat_diff.extend(nested_flat_diff)
-        else:
-            if key not in d1:
-                diff[f'+ {key}'] = val2
-                flat_diff.append(
-                    f"Property '{plain.format_path(d2[key], d2)}' "
-                    f"was added with value: {plain.format_value(val2)}\n"
-                )
-            elif key not in d2:
-                diff[f'- {key}'] = val1
-                flat_diff.append(
-                    f"Property '{plain.format_path(d1[key], d1)}' "
-                    f"was removed\n"
-                )
-            elif val1 == val2:
-                diff[f'  {key}'] = val1
-            elif val1 != val2:
-                diff[f'- {key}'] = val1
-                diff[f'+ {key}'] = val2
-                flat_diff.append(
-                    f"Property '{plain.format_path(d1[key], d1)}' "
-                    f"was updated. From {plain.format_value(val1)} "
-                    f"to {plain.format_value(val2)}\n"
-                )
-
-    return stylish.stylize(diff), ''.join(flat_diff)
+        elif get_meta(val1, val2) == 'original':
+            diff[key] = 'original'
+        elif get_meta(val1, val2) == 'changed':
+            diff[key] = 'changed'
+    return diff
 
 
-def generate_diff(file1: Path, file2: Path, style: str = 'stylish') -> str:
-    data1, data2 = get_data(file1, file2)
-    stylish_diff, plain_diff = get_diff(data1, data2)
+def generate_diff(path_file1: Path, path_file2: Path,
+                  style: str = 'stylish') -> str:
+    data1 = get_data(path_file1)
+    data2 = get_data(path_file2)
+    diff = get_diff(data1, data2)
     if style == 'stylish':
-        return stylish_diff
+        stylish_diff_dict = stylish.format_stylish_diff(diff, data1, data2)
+        return stylish.stylize(stylish_diff_dict)
     elif style == 'plain':
+        plain_diff = plain.format_plain_diff(diff, data1, data2,
+                                             path_file1, path_file2)
         return plain_diff
     elif style == 'json':
         pass
