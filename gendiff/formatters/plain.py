3ccded40f5fd9c49e79cdc30269dcceac9c920c2
MaxKtv <maximkutiaev@outlook.com>
2024-04-24 17:12:04 +1000

    fix formatters: plain.py, stylish.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,12 +1,63 @@
-def format_value(value):
+from gendiff.parser import get_data
+from typing import Dict, Any
+from pathlib import Path
+
+
+def format_plain_diff(diff: Dict[str, Any],
+                      data1: Dict[str, Any], data2: Dict[str, Any],
+                      path_file1: Path, path_file2: Path) -> str:
+    text = []
+    for key in sorted(diff):
+        if isinstance(diff[key], dict):
+            nested_diff = format_plain_diff(diff[key], data1[key], data2[key],
+                                            path_file1, path_file2)
+            if nested_diff:
+                text.append(nested_diff)
+        else:
+            make_plain_diff(diff, key, data1, data2,
+                            path_file1, path_file2, text)
+    return '\n'.join(text)
+
+
+def make_plain_diff(diff: Dict[str, Any], key: str,
+                    data1: Dict[str, Any], data2: Dict[str, Any],
+                    path_file1: Path, path_file2: Path, text: list) -> str:
+    full_data1 = get_data(path_file1)
+    full_data2 = get_data(path_file2)
+    if diff[key] == 'changed':
+        if key not in data2:
+            text.append(f"Property '{format_path(key, full_data1)}' "
+                        f"was removed")
+        elif key not in data1:
+            text.append(
+                f"Property '{format_path(key, full_data2)}' was added "
+                f"with value: {format_value(data2[key])}"
+            )
+        else:
+            text.append(
+                f"Property '{format_path(key, full_data1)}' was updated. "
+                f"From {format_value(data1[key])} "
+                f"to {format_value(data2[key])}")
+    return text
+
+
+def format_value(value: Any) -> str:
     if isinstance(value, dict):
         return '[complex value]'
     if value is None:
-        value = 'null'
+        return 'null'
     elif isinstance(value, bool):
-        value = str(value).lower()
+        return str(value).lower()
     return f"'{value}'" if isinstance(value, str) else str(value)
 
 
-def format_path(target, data):
-    pass
+def format_path(endpoint: str, data: Dict[str, Any],
+                path: list = []) -> str or None:
+    if endpoint in data:
+        return '.'.join(path + [endpoint])
+    for key, value in data.items():
+        if isinstance(value, dict):
+            nested_path = format_path(endpoint, value, path + [key])
+            if nested_path:
+                return nested_path
+    return None
--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,26 +1,64 @@
-def stylize(obj: dict, replacer: str = '    ', spaces_count: int = 1) -> str:
-    def stringify_value(value, indent: str):
-        if isinstance(value, (str, int, float)):
-            return value
-        elif isinstance(value, bool):
-            return str(value).lower()
-        elif value is None:
-            return 'null'
-        elif isinstance(value, dict):
+from typing import Dict, Any
+
+
+def format_stylish_diff(diff: Dict[str, Any], data1: Dict[str, Any],
+                        data2: Dict[str, Any]) -> Dict[str, Any]:
+    formatter_dict = {}
+    for key in sorted(diff):
+        if isinstance(diff[key], dict):
+            nested_diff = format_stylish_diff(diff[key], data1[key], data2[key])
+            if nested_diff:
+                formatter_dict[key] = nested_diff
+        else:
+            make_stylish_diff(diff, key, data1, data2, formatter_dict)
+    return formatter_dict
+
+
+def make_stylish_diff(diff: Dict[str, Any], key: str,
+                      data1: Dict[str, Any], data2: Dict[str, Any],
+                      dictionary: Dict[str, Any]) -> Dict[str, Any]:
+    if diff[key] == 'original':
+        dictionary[f'  {key}'] = data1[key]
+    elif diff[key] == 'changed':
+        if key not in data2:
+            dictionary[f'- {key}'] = data1[key]
+        elif key not in data1:
+            dictionary[f'+ {key}'] = data2[key]
+        else:
+            dictionary[f'- {key}'] = data1[key]
+            dictionary[f'+ {key}'] = data2[key]
+    return dictionary
+
+
+def decoder(obj: Any) -> str:
+    if isinstance(obj, str | int | float):
+        return obj if not isinstance(obj, bool) else str(obj).lower()
+    elif obj is None:
+        return 'null'
+    else:
+        raise TypeError(f'Type {type(obj)} not supported')
+
+
+def stylize(obj: Dict[str, Any],
+            replacer: str = ' ', spaces_count: int = 2) -> str:
+
+    def stringify_value(value: Any, indent: str) -> str:
+        if not isinstance(value, dict):
+            return decoder(value)
+        else:
             lines = []
             for key, val in value.items():
-                format_value = stringify_value(val,
-                                               indent + replacer * spaces_count)
+                inborn_indent_count = 2
+                format_value = stringify_value(val, indent + replacer
+                                               * spaces_count
+                                               * inborn_indent_count)
                 lines.append(
-                    f'{indent + replacer * spaces_count}{key}: '
-                    f'{format_value if not isinstance(format_value, bool) else str(format_value).lower()}'
+                    f'{indent}'
+                    f'{key if not key[0].isalpha() else f"  {key}"}:'
+                    f'{" " if len(str(format_value)) >= 1 else ""}'
+                    f'{format_value}'
                 )
             if lines:
-                return (
-                        '{\n' + '\n'.join(lines) + '\n' + indent + '}'
-                )
-
-        else:
-            pass
-
+                return ('{\n' + '\n'.join(lines) + '\n'
+                        + indent[:-spaces_count * len(replacer)] + '}')
     return stringify_value(obj, replacer * spaces_count)
