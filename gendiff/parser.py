724834b23c7d2336112938add932a628194eae13
MaxKtv <maximkutiaev@outlook.com>
2024-04-25 16:27:21 +1000

    refactor parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,10 +3,10 @@ from yaml import safe_load
 from pathlib import Path
 from typing import Dict, Any
 
-EXTENSION = {'.json': load, '.yaml': safe_load, '.yml': safe_load}
+EXTENSIONS = {'.json': load, '.yaml': safe_load, '.yml': safe_load}
 
 
-def get_file_extension(file) -> str:
+def get_file_extension(file: Path) -> str:
     file = Path(file)
     return file.suffix
 
@@ -14,8 +14,8 @@ def get_file_extension(file) -> str:
 def get_data(file: Path) -> Dict[str, Any]:
     with open(file) as open_file:
         ext = get_file_extension(file)
-        if ext in EXTENSION:
-            data = EXTENSION[ext](open_file)
+        if ext in EXTENSIONS:
+            data = EXTENSIONS[ext](open_file)
             return data
         else:
             raise ValueError('Oh no, something went wrong'
