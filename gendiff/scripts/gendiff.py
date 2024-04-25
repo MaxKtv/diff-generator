02f7157f578d16a15402f187c35a63ed0cb3af52
MaxKtv <maximkutiaev@outlook.com>
2024-04-25 16:29:22 +1000

    feat: add format 'json': gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,7 @@ parser.add_argument('first_file')
 parser.add_argument('second_file')
 parser.add_argument('-f', '--format', help='set format of output',
                     default='stylish',
-                    choices=['stylish', 'plain'])
+                    choices=['stylish', 'plain', 'json'])
 
 args = parser.parse_args()
 
