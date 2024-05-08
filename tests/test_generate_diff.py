e7924f0ad557707a942be45384d1acac84702f27
MaxKtv <maximkutiaev@outlook.com>
2024-05-08 15:32:44 +1000

    tests test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -18,6 +18,7 @@ def test_generate_stylish_diff():
     with open(expected, 'r') as e:
         assert generate_diff.generate_diff(file1_y, file2_y) == e.read()
 
+
 def test_generate_plain_diff():
     file1_j, file1_y = get_path('file1.json'), get_path('file1.yml')
     file2_j, file2_y = get_path('file2.json'), get_path('file2.yaml')
@@ -26,3 +27,13 @@ def test_generate_plain_diff():
         assert generate_diff.generate_diff(file1_j, file2_j, 'plain') == e.read()
     with open(expected, 'r') as e:
         assert generate_diff.generate_diff(file1_y, file2_y, 'plain') == e.read()
+
+
+def test_generate_json_diff():
+    file1_j, file1_y = get_path('file1.json'), get_path('file1.yml')
+    file2_j, file2_y = get_path('file2.json'), get_path('file2.yaml')
+    expected = get_path('expected_json.json')
+    with open(expected, 'r') as e:
+        assert generate_diff.generate_diff(file1_j, file2_j, 'json') == e.read()
+    with open(expected, 'r') as e:
+        assert generate_diff.generate_diff(file1_y, file2_y, 'json') == e.read()
