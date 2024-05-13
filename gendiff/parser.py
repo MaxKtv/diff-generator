b102311ca054b5264173ec19b64d46592fcdaabe
MaxKtv <maximkutiaev@outlook.com>
2024-05-13 16:08:27 +1000

    refactor/fix parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,25 +3,21 @@ from yaml import safe_load
 from pathlib import Path
 from typing import Dict, Any
 
+
 EXTENSIONS = {
-    '.json': load,
-    '.yaml': safe_load,
-    '.yml': safe_load
+    'json': load,
+    'yaml': safe_load,
+    'yml': safe_load
 }
 
 
-def get_file_extension(file: Path) -> str:
-    file = Path(file)
-    return file.suffix
-
-
-def get_data(file: Path) -> Dict[str, Any]:
+def get_data(file: Path, ext: str) -> Dict[str, Any]:
     with open(file) as open_file:
-        ext = get_file_extension(file)
         if ext in EXTENSIONS:
             data = EXTENSIONS[ext](open_file)
             return data
         else:
-            raise ValueError('Oh no, something went wrong'
-                             '\nSupported extensions are:'
-                             '\n>>> .json, .yaml and .yml')
+            extensions = ', '.join(EXTENSIONS.keys())
+            raise ValueError(f'Oh no, something went wrong'
+                             f'\nSupported extensions are:'
+                             f'\n>>> {extensions}')
