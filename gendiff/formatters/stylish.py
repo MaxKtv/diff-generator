b9f49fc21988b0072a80fbd540d5f93e022bedf3
MaxKtv <maximkutiaev@outlook.com>
2024-05-21 15:18:42 +1000

    fix/refactor stylsih.py

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,106 +1,94 @@
+from itertools import chain
 from typing import Dict, Any, Tuple
 
+
+REPLACER = ' '
+SPACE_COUNT = 4
 STYLISH_META = {
     'added': '+ ',
     'original': '  ',
     'removed': '- ',
-    'updated': ('- ', '+ ')
+    'updated': ('- ', '+ '),
+    'nested': '  '
 }
+INBORN_INDENT = len(STYLISH_META[next(iter(STYLISH_META))])
 
 
-def format_stylish_diff(diff: Dict[str, Tuple[str, Any]]) -> Dict[str, Any]:
+def stringify_value(node: Any, depth: int = 0) -> str:
     """
-     Formats diff dictionary to "stylish" style dictionary
+    Turns node to "stylish" style string
 
-     Args:
-         diff (Dict[str, Tuple[str, Any]]): Dictionary of difference
-                                            data1 and data2
+    Args:
+        node (Any): variable to be stringified
+        depth (int, optional): Dictionary's level of depth. Defaults to 0.
 
-     Returns:
-         Dict[str, Any]: Dictionary of difference data1 and data2
-                         in "stylish" style
-     """
-    formatter_dict = {}
-    for key, [meta, value] in diff.items():
-        if meta == 'nested':
-            nested = format_stylish_diff(value)
-            if nested:
-                formatter_dict[key] = nested
-        elif meta == 'updated':
-            data1_value, stylish_meta1 = value[0], STYLISH_META[meta][0]
-            data2_value, stylish_meta2 = value[1], STYLISH_META[meta][1]
-            formatter_dict[f'{stylish_meta1}{key}'] = data1_value
-            formatter_dict[f'{stylish_meta2}{key}'] = data2_value
+    Returns:
+        str: "stylish" style difference
+    """
+    if not isinstance(node, dict):
+        return normalize_value(node, depth)
+    lines = []
+    indent = get_indents(depth)
+    base_indent = get_indents(depth + 1)
+    for key, value in sorted(node.items()):
+        if isinstance(value, tuple):
+            meta, val = value
+            if meta == 'updated':
+                old_val, new_val = val
+                lines.append(f"{base_indent}{STYLISH_META['removed']}{key}: "
+                             f"{normalize_value(old_val, depth + 1)}")
+                lines.append(f"{base_indent}{STYLISH_META['added']}{key}: "
+                             f"{normalize_value(new_val, depth + 1)}")
+            else:
+                formatted_value = stringify_value(val, depth + 1) \
+                    if meta == 'nested' else normalize_value(val, depth + 1)
+                lines.append(f"{base_indent}{STYLISH_META[meta]}{key}: "
+                             f"{formatted_value}")
         else:
-            formatter_dict[f'{STYLISH_META[meta]}{key}'] = value
+            lines.append(f"{base_indent}{STYLISH_META['original']}{key}: "
+                         f"{normalize_value(value, depth + 1)}")
+    result = chain(
+        '{', lines, [
+            (indent + '}') if len(indent) < 1 else (indent + '  ' + '}')
+        ]
+    )
+    return '\n'.join(result)
 
-    return formatter_dict
 
-
-def decoder(obj: Any) -> str:
+def normalize_value(obj: Any, depth: int) -> str:
     """
     Converts object(values) to string
 
     Args:
         obj (Any): Object to be converted
+        depth (int, optional): Dictionary's level of depth
 
     Returns:
         str: Converted string
     """
-    if isinstance(obj, str | int | float):
-        return obj if not isinstance(obj, bool) else str(obj).lower()
+    if isinstance(obj, bool):
+        return str(obj).lower()
     elif obj is None:
         return 'null'
-    else:
-        return obj
+    elif isinstance(obj, (str | int | float)):
+        return str(obj)
+    elif isinstance(obj, dict):
+        return stringify_value(obj, depth)
 
 
-def stringify_stylish_diff(formatter_dict: Dict[str, Any],
-                           replacer: str = ' ', spaces_count: int = 2) -> str:
+def get_indents(depth: int) -> str:
     """
-    Turns dictionary to "stylish" style string
+    Builds indent in accordance with depth
 
     Args:
-        formatter_dict (Dict[str, Any]): formatted dictionary of difference
-        replacer (str, optional): replacement character. Defaults to ' '.
-        spaces_count (int, optional): number of spaces to replace.
-                                      Defaults to 2.
+        depth (int, optional): Dictionary's level of depth
 
     Returns:
-        str: "stylish" style difference
+        str: indent
     """
-    def stringify_value(value: Any, indent: str) -> str:
-        """
-        Turns value of dictionary to "stylish" style value
-
-        Args:
-            value (Any): Value to be converted
-            indent (str): indentation
-
-
-        Returns:
-            str: "stylish" styled value
-        """
-        if not isinstance(value, dict):
-            return decoder(value)
-        else:
-            lines = []
-            for key, val in value.items():
-                inborn_indent_count = 2
-                format_value = stringify_value(val, indent + replacer
-                                               * spaces_count
-                                               * inborn_indent_count)
-                lines.append(
-                    f'{indent}'
-                    f'{key if not key[0].isalpha() else f"  {key}"}:'
-                    f'{" "}'
-                    f'{format_value}'
-                )
-            if lines:
-                return ('{\n' + '\n'.join(lines) + '\n'
-                        + indent[:-spaces_count * len(replacer)] + '}')
-
-    return stringify_value(formatter_dict, replacer * spaces_count)
+    if depth <= 0:
+        return ''
+    return REPLACER * (depth * SPACE_COUNT - INBORN_INDENT)
 
 
 def get_stylish_diff(diff: Dict[str, Tuple[str, Any]]) -> str:
@@ -108,11 +96,9 @@ def get_stylish_diff(diff: Dict[str, Tuple[str, Any]]) -> str:
     Formats diff dictionary to "stylish" style string
 
     Args:
-        diff (Dict[str, Tuple[str, Any]]): Dictionary of difference
+        diff (Dict[str, Tuple[str, Tuple[str, Any]]): Dictionary of difference
 
     Returns:
         str: "stylish" styled difference
     """
-    formatter_dict = format_stylish_diff(diff)
-    stylish_diff = stringify_stylish_diff(formatter_dict)
-    return stylish_diff
+    return stringify_value(diff)
