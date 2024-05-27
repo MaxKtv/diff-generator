94d1b7f96f8d0ea975759f175a1d075c40c0de1b
MaxKtv <maximkutiaev@outlook.com>
2024-05-27 14:39:03 +1000

    refactor gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,17 @@
 import sys
-from gendiff.cli import create_parser
+from gendiff.cli import argumentize
 from gendiff.diff_generator import generate_diff
+
+ERRORS = (ValueError, KeyError, FileNotFoundError)
 
 
 def main():
     try:
-        args = create_parser()
+        args = argumentize()
         print(generate_diff(args.first_file, args.second_file, args.format))
         sys.exit(0)
-    except Exception as e:
-        print(e)
+    except ERRORS as e:
+        print(f"An ERROR occurred: {e}")
         sys.exit(1)
 
 
